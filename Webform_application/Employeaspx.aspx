550c5f9dd75d90760573c88dae9a46f5e4b4c189
Anshika0219 <anshikaa@moback.com>
2022-10-31 08:00:57 +0530

    WebForm_CrudApplication

---
+++ Webform_application/Employeaspx.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Employeaspx.aspx.cs" Inherits="Employeaspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 115px;
+        }
+        .auto-style2 {
+            margin-left: 57px;
+        }
+        .auto-style3 {
+            margin-left: 111px;
+        }
+        .auto-style4 {
+            margin-left: 77px;
+        }
+        .auto-style5 {
+            margin-left: 66px;
+        }
+        .auto-style6 {
+            margin-left: 126px;
+        }
+        .auto-style7 {
+            margin-left: 98px;
+        }
+        .auto-style8 {
+            margin-left: 140px;
+        }
+        .auto-style9 {
+            margin-left: 179px;
+        }
+        .auto-style10 {
+            margin-left: 409px;
+        }
+        .auto-style11 {
+            width: 254px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                     <tr><td>Employe Name<asp:TextBox ID="EmployeName" runat="server" CssClass="auto-style5"></asp:TextBox>
+                         <br />
+                         </td> </tr>
+                     <tr><td>Employe Age<asp:TextBox ID="Employeage" runat="server" CssClass="auto-style4" OnTextChanged="Employeage_TextChanged"></asp:TextBox>
+                         <br />
+                         </td> </tr>
+                   <tr><td>EmailId <asp:TextBox ID="emailId" runat="server" CssClass="auto-style1"></asp:TextBox>
+                       <br />
+                       </td> </tr>
+                     <tr><td>Password<asp:TextBox ID="password" runat="server" CssClass="auto-style3"></asp:TextBox>
+                         <br />
+                         </td> </tr>
+              <tr><td>Contact Number<asp:TextBox ID="contactnumber" runat="server" CssClass="auto-style2"></asp:TextBox>
+                  <br />
+                  </td> </tr>
+                <tr><td> Sallery<asp:TextBox ID="Sallery" runat="server" CssClass="auto-style6"></asp:TextBox>
+                    <br />
+                    <br />
+                    </td></tr>
+               <tr><td><asp:Button ID="Insert" runat="server" Text="Insert" OnClick="Insert_Click" CssClass="auto-style10" Width="98px"/></td>
+                   <td class="auto-style11"><asp:Button ID="Read" runat="server" Text="Read" OnClick="Read_Click" CssClass="auto-style9" Width="59px" /></td>
+                 <td><asp:Button ID="Update" runat="server" Text="update" OnClick="Update_Click" CssClass="auto-style8" Width="74px" /></td>
+                   <td><asp:Button ID="Delete" runat="server" Text="Delete" OnClick="Delete_Click" CssClass="auto-style7" /></td>
+               </tr>
+                    <tr><td>
+                        <br />
+                        <br />
+                        <br />
+                        <asp:Label ID="Message" runat="server" Text="Message"></asp:Label>
+                        <br />
+                        </td></tr>
+                   </table>
+            </div>
+    </form>
+</body>
+</html>
---
+++ Webform_application/Employeaspx.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+using System.Data;
+
+public partial class Employeaspx : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection("Data Source=DESKTOP-UQON0I1;Initial Catalog=company_Hr;Integrated Security=True");
+
+ 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      
+    }
+
+    protected void Insert_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            con.Open();
+            string insertquery = "Exec SP_InsertEmployeinfo '" + EmployeName.Text + "','" + Employeage.Text + "','" + emailId.Text + "' , '" + password.Text + "','" + contactnumber.Text + "','" + Sallery.Text + "'";
+            SqlCommand insertcommand = new SqlCommand(insertquery, con);
+            insertcommand.ExecuteNonQuery();
+            Message.Text = "Your Data inserted successfully";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+    protected void Read_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            con.Open();
+            string displayquery = "select * from SP_SelectEmployeinfo";
+            SqlCommand displayCommand = new SqlCommand(displayquery, con);
+            SqlDataReader dataReader = displayCommand.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Console.WriteLine("Employe Name : " + dataReader.GetValue(0).ToString());
+                Console.WriteLine("Employe Age : " + dataReader.GetValue(1).ToString());
+                Console.WriteLine("EmailId : " + dataReader.GetValue(2).ToString());
+                Console.WriteLine("ContactNumber: " + dataReader.GetValue(3).ToString());
+                Console.WriteLine("Sallery: " + dataReader.GetValue(4).ToString());
+            }
+            Message.Text = "your data display Successfully";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally {
+            con.Close();
+        }
+      }
+    protected void Update_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            con.Open();
+            string updatequery = "Exec SP_UpdateEmployeInfo '" + EmployeName.Text + "','" + Employeage.Text + "','" + emailId.Text + "','" + password.Text + "','" + contactnumber.Text + "','" + Sallery.Text + "'";
+            SqlCommand updatecommand = new SqlCommand(updatequery, con);
+            updatecommand.ExecuteNonQuery();
+            Message.Text = "your data updated Successfully";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+    protected void Delete_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            con.Open();
+            string deletequery = "Exec SP_DeleteEmployeinfo '" + emailId.Text + "' ";
+            SqlCommand deletecommand = new SqlCommand(deletequery, con);
+            deletecommand.ExecuteNonQuery();
+            Message.Text = "your data deleted successfully";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    protected void Employeage_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
